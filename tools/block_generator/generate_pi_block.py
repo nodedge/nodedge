a601d4982bffa935b423cf5a811d037443e19e71
Anthony De Bortoli <anthony.debortoli@protonmail.com>
2020-07-26 23:15:06 +0200

    Minor fixes in code generation

--- tools/block_generator/generate_pi_block.py
+++ tools/block_generator/generate_pi_block.py
@@ -3,18 +3,18 @@ import os.path
 from string import Template
 
 config_file = "block_config.csv"
-save_path = "./generated_blocks/"
+save_path = "../../nodedge/tools/block_generator/generated_blocks/"
 
 if __name__ == "__main__":
 
     # Read one line of the csv
-    with open(config_file, mode="r") as infile:
+    with open(config_file) as infile:
         reader = csv.DictReader(infile, delimiter=",")
 
         for row in reader:
 
             # Generate code
-            with open("pi_block_template.txt", "r") as template_file:
+            with open("pi_block_template.txt") as template_file:
                 input_data = template_file.read()
             template = Template(input_data)
 
