3a69c02d63126af7464c18cc2635eb19ea53ca67
Anthony De Bortoli <anthony.debortoli@protonmail.com>
2022-10-27 21:24:54 +0200

    WIP create class template.

---
+++ nodedge/elements/comment_element.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+comment_element.py module containing :class:`~nodedge.comment_element.py.CommentElement` class.
+"""
+
+
+class CommentElement(Element):
+    def __init__(self):
+        pass
---
+++ nodedge/elements/element.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+element.py module containing :class:`~nodedge.element.py.Element` class.
+"""
+from nodedge.serializable import Serializable
+
+
+class Element(Serializable):
+    def __init__(self):
+        pass
