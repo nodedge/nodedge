f0e6e193fd6cddbeedccef7d02adb3cb7ddc6d8c
don4get <anthony.debortoli@protonmail.com>
2022-11-19 15:41:56 +0100

    Bump to 0.3.0

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
     keywords=KEYWORDS,
     description="Graphical editor for physical modeling and simulation.",
     url="https://www.nodedge.io",
-    version="0.2.3",
+    version="0.3.0",
     license="MIT",
     author="Anthony De Bortoli",
     author_email="anthony.debortoli@nodedge.io",
