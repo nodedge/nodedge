717107fd01f40bbc4f9c85ca13808fda65d8732d
Enrica Soria <enrica.s92@gmail.com>
2022-11-21 21:05:00 +0100

    Refactor scipy integration examples

--- demos/example_scipy_integrate.py
+++ demos/example_scipy_integrate.py
@@ -1,19 +1,20 @@
 from math import *
-
 from pylab import *
 from scipy.integrate import quad
 
 
-# the function to be integrated:
+# A function to be integrated
 def my_func(x):
     return cos(x)
 
 
-x = arange(-pi, pi + 0.1, 0.1)
-y_sin = [sin(el) for el in x]
-y_cos_integrated = [quad(my_func, -pi, el)[0] - sin(pi) for el in x]
+if __name__ == "__main__":
+    dx = 0.1
+    x = arange(-pi, pi + dx, dx)
+    y_sin = [sin(el) for el in x]
+    y_cos_integrated = [quad(my_func, -pi, el)[0] - sin(pi) for el in x]
 
-plot(x, y_sin, ".-")
-plot(x, y_cos_integrated)
-legend(["Sin", "Cos integrated"])
-show()
+    plot(x, y_sin, ".-")
+    plot(x, y_cos_integrated)
+    legend(["Sin", "Integrated cos"])
+    show()
--- demos/example_scipy_odeint.py
+++ demos/example_scipy_odeint.py
@@ -6,31 +6,38 @@ A spring-mass system with damping
 """
 
 
-def damped_osc(u, t):  # defines the system of diff. eq.
-    x, v = u
-    return (v, -k * (x - L) / m - b * v)  # the vector (dx/dt, dv/dt)
+# Define the system of differential equation
+def damped_oscillator(x, t):
+    p, v = x
+    # Return the derivative vector (dp/dt, dv/dt)
+    return (v, -k * (p - L) / m - b * v)
 
 
-t = arange(0, 20.1, 0.1)
-u0 = array([1, 0])  # initial values of x, v
+if __name__ == "__main__":
+    # Time vector
+    t = arange(0, 20.1, 0.1)
 
-# assume certain values of parameters b, k, L, m; these would be given in the problem
-b = 0.4
-k = 8.0
-L = 0.5
-m = 1.0
+    # Initial values of p, v
+    x0 = array([1, 0])
 
-u = odeint(damped_osc, u0, t)  # solve ODE
-# plot x, v, phase, using matplotlib
+    # Assume certain values of parameters b, k, L, m; these would be given in the problem
+    b = 0.4
+    k = 8.0
+    L = 0.5
+    m = 1.0
 
-figure(1)
-plot(t, u[:, 0], t, u[:, 1])  # u[:,0] is x; u[:,1] is v
-xlabel("Time")
-title("Damped Oscillator")
+    x = odeint(damped_oscillator, x0, t)  # solve ODE
 
-figure(2)
-plot(u[:, 0], u[:, 1])
-title("Phase-space")
-xlabel("Position")
-ylabel("Velocity")
-show()
+    # Plot p, v, phase, using matplotlib
+    figure(1)
+    plot(t, x[:, 0], t, x[:, 1])  # x[:,0] is p; u[:,1] is v
+    xlabel("Time")
+    title("Damped oscillator")
+
+    figure(2)
+    plot(x[:, 0], x[:, 1])
+    title("Phase-space")
+    xlabel("Position")
+    ylabel("Velocity")
+
+    show()
