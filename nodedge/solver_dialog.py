1bea1224e81fefbaacdf15aea7192a3c1c34be34
Nodedge <admin@nodedge.io>
2023-01-23 20:48:30 +0100

    Remove unused solver options.

--- nodedge/solver_dialog.py
+++ nodedge/solver_dialog.py
@@ -41,7 +41,7 @@ class SolverDialog(QDialog):
         self.configLayout = QFormLayout()
         self.configFrame.setLayout(self.configLayout)
         self.solverCombo = QComboBox()
-        self.solverCombo.addItems(["No Solver", "Solver2", "Solver3"])
+        self.solverCombo.addItems(["Basic solver"])
         self.solverCombo.currentIndexChanged.connect(self.updateSolverConfig)
 
         self.solverOptions = QLineEdit()
@@ -51,9 +51,9 @@ class SolverDialog(QDialog):
         self.toleranceSpinBox = QDoubleSpinBox()
         self.finalTimeEdit = QLineEdit()
 
-        self.configLayout.addRow("Solver Name", self.solverName)
+        self.configLayout.addRow("Solver name", self.solverName)
         self.configLayout.addRow("Solver", self.solverCombo)
-        self.configLayout.addRow("Solver Options", self.solverOptions)
+        self.configLayout.addRow("Solver options", self.solverOptions)
         self.configLayout.addRow("Time step", self.timestepSpinBox)
         self.configLayout.addRow("Max iterations", self.maxIterationsSpinBox)
         self.configLayout.addRow("Tolerance", self.toleranceSpinBox)
